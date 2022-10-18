fe8570cdbf4d82faa9e58e3b0c86fb72ad3bfcd9
MarcoDias <marcomartinsdias@gmail.com>
2022-10-18 15:11:31 +0100

    test commit

--- mock.js
+++ mock.js
@@ -1,5 +1,9 @@
 
-users =[{nome:"Joao", idade:12},{nome:"rita", idade:2}]
+users =[
+    {nome:"Joao", idade:12},
+    {nome:"rita", idade:2},
+    {nome:"Berto", idade:5}
+]
 
 
 exports.getUsers = users;
